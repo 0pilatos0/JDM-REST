46fbed4995c553b66d32674ba2e74023238607c0
0pilatos0 <Paulvanderlei@hotmail.nl>
2022-09-20 13:05:34 +0200

    Setting up the project

---
+++ src/main/java/com/pilatos/jdmrest/JdmRestApplication.java
@@ -0,0 +1,13 @@
+package com.pilatos.jdmrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdmRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdmRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pilatos/jdmrest/JdmRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pilatos.jdmrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdmRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
