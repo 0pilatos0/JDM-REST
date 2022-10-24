bb819edc15c8daaaf9d64081efdfb2b450bdc594
0pilatos0 <Paulvanderlei@hotmail.nl>
2022-10-24 17:57:45 +0200

    + added javaDoc

--- src/main/java/com/avd/jdmrest/controller/AbstractController.java
+++ src/main/java/com/avd/jdmrest/controller/AbstractController.java
@@ -3,10 +3,19 @@ package com.avd.jdmrest.controller;
 abstract class AbstractController {
     String prefix = "[REQUEST] ";
 
+    /**
+     * Log the controller which is called
+     * @param message
+     */
     public void log(String message) {
         System.out.println(prefix + message);
     }
 
+    /**
+     * Log the controller which is called with id
+     * @param message
+     * @param id
+     */
     public void log(String message, Long id) {
         System.out.println(prefix + message + " " + id);
     }
